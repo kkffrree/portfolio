21de3dcfd8cbd9b074562c60ba45f4bda3d50850
uroegi <yattaruchan6@yahoo.co.jp>
2025-10-24 22:54:47 +0900

    rewrite dbconnect

--- mmb/dbconnect.php
+++ mmb/dbconnect.php
@@ -1,7 +1,10 @@
 <?php
 try {
-    $db = new PDO('mysql:dbname=mini_bbs;host=localhost;charset=utf8', 'root', 'root');
+    // dbconnect.php と database.sqlite が同じ mmb フォルダ内にある場合
+    $db = new PDO('sqlite:' . __DIR__ . '/database.sqlite');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo 'DB接続エラー：' , $e->getMessage();
+    echo 'DB接続エラー：' . $e->getMessage();
+    exit;
 }
 ?>
