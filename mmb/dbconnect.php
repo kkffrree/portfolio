b9281535141a8044cf87b8f022aaf117f569ef6a
uroegi <yattaruchan6@yahoo.co.jp>
2025-11-02 01:05:18 +0900

    update

--- mmb/dbconnect.php
+++ mmb/dbconnect.php
@@ -1,9 +1,9 @@
 <?php
 try {
   $db = new PDO(
-    'mysql:dbname=LAA1686811-alftte;host=mysql80-3.lolipop.jp;charset=utf8',
+    'mysql:dbname=LAA1686811-alftte;host=mysql80-1.lolipop.lan;charset=utf8',
     'LAA1686811',
-    'YdV0kMCESrFVyL0C'
+    'kokiri27'
   );
 } catch (PDOException $e) {
   echo 'DB接続エラー: ' . $e->getMessage();
