42dd5313abcbc3869a528fbe7352501e61cd17b1
uroegi <yattaruchan6@yahoo.co.jp>
2025-11-02 00:42:12 +0900

    update

--- mmb/dbconnect.php
+++ mmb/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 try {
   $db = new PDO(
-    'mysql:dbname=LAA1686811-alftte;host=mysql80-3.lolipop.lan;charset=utf8',
+    'mysql:dbname=LAA1686811-alftte;host=mysql80-3.lolipop.jp;charset=utf8',
     'LAA1686811',
     'YdV0kMCESrFVyL0C'
   );
